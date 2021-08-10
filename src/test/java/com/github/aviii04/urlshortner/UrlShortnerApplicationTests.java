9699fbb548a30b178d1a80180d5eff1e6e4dd5db
Avinash Thakur <imthakuravinash@gmail.com>
2021-08-10 17:07:19 +0530

    Adding default maven project structure & spring boot starter dependencies.

---
+++ src/test/java/com/github/aviii04/urlshortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aviii04.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
